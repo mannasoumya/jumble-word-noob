8fa2c777418b89c9decf367fa3d68da05a1c8465
Manna Soumya <sm323265@gmail.com>
2020-08-13 10:43:54 +0530

    Removed unnecessary end lines and added comments

--- jumble_word.py
+++ jumble_word.py
@@ -4,26 +4,24 @@ import time
 
 in_word = sys.argv[1]
 already_seen = []
+#already seen is already found de jumbled word
 if len(sys.argv) > 2:
     already_seen = sys.argv[2:]
-
+#read from english word dictionary
 eng_dict = open("english3.txt", "r").read().split("\n")
+#generator for english word dictionary
 eng_dict_gen = (x for x in eng_dict)
+#finding all permutations of input word
 p = permutations(in_word)
 found = False
-
 perm_arr = []
-
 for tup in list(p):
     perm_arr.append(''.join(tup).lower())
-
+#sorting parmutations and removing duplicates
 perm_arr.sort()
-
 perm_arr = list(dict.fromkeys(perm_arr))
 count = 0
 t1 = time.time()
-
-
 while(count < len(eng_dict)):
     if found == False:
         w = str(next(eng_dict_gen))
